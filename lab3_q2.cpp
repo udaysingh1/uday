d4d94a48485eca246d8736ef37dca6250c1bc45f
udaysingh1 <42139575+udaysingh1@users.noreply.github.com>
2018-08-16 15:06:13 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,17 @@
+//Q. add two numbers and print their sumon the terminal
+
+//include the library iostream
+#include<iostream>
+using namespace std;
+//write the main function
+int main() {
+int a,b,sum,difference,multiply,modulus;
+
+ a=1;
+ b=2;
+ sum= a + b;
+//print their sum
+cout << "the sum of " << a << " and " <<b<< "is "<< sum << endl ;
+return 0;
+}
+
