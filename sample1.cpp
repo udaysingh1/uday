e0c5e69b91b62190707f233e761404667a6416cc
udaysingh1 <42139575+udaysingh1@users.noreply.github.com>
2018-08-16 14:41:19 +0000

    Add files via upload

---
+++ sample1.cpp
@@ -0,0 +1,19 @@
+//include library
+#include <iostream>
+using namespace std;
+int main()
+//process
+{
+//declaring variable
+int a=1;
+char b='2';
+float c=2.007;
+double d=2.01;
+bool e=0;
+//will get size of variable
+cout <<"size of char:"<<sizeof(char)<<" byte"<<" contents of a="<<a<<endl;
+cout <<"size of int:"<<sizeof(int)<<" byte"<<" contents of b="<<b<<endl;
+cout <<"size of float:"<<sizeof(float)<<" byte"<<" contents of c="<<c<<endl;
+cout <<"size of double:"<<sizeof(double)<<" byte"<<" contents of d="<<d<<endl;
+return 0;
+}
